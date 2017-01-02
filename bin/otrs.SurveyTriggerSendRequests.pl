5e036afefad4bbff769e1fb10fb03701a68d5af7
Manuel Hecht <manuel.hecht@otrs.com>
2017-01-02 11:58:48 +0100

    Updated copyright.

--- bin/otrs.SurveyTriggerSendRequests.pl
+++ bin/otrs.SurveyTriggerSendRequests.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # --
-# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -51,7 +51,7 @@ if (
 {
     print <<EOF;
 otrs.SurveyTriggerSendRequests.pl - Trigger sending delayed survey requests
-Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
 
 Usage:
 
